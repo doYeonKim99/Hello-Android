4237561ae9d207654b375f124d568e98fbfe2acd
Doyeon Kim <ehdus859599@gmail.com>
2016-03-22 21:05:08 +0900

    #1 Import project

---
+++ app/src/main/java/kr/hs/emirim/dok96/helloandriod/mainactivity.java
@@ -0,0 +1,13 @@
+package kr.hs.emirim.dok96.helloandriod;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+public class mainactivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mainactivity); //r 은 리소스
+    }
+}
